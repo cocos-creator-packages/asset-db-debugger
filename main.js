6fdef651f850c1429ebfc6446e7037c3c14129df
Johnny Wu <KarasAya@gmail.com>
2016-01-09 18:08:46 +0800

    update for electron 0.36

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
+'use strict';
+
 module.exports = {
-    load: function () {
-    },
+  load () {
+  },
 
-    unload: function () {
-    },
+  unload () {
+  },
 
-    'asset-db-debugger:open': function (uuid) {
-        Editor.Panel.open('asset-db-debugger.panel');
-    },
+  'asset-db-debugger:open' (event, uuid) {
+    Editor.Panel.open('asset-db-debugger.panel');
+  },
 
-    'asset-db-debugger:query-info': function ( reply ) {
-        var results = [];
-        for ( var p in Editor.assetdb._path2uuid ) {
-            var url = Editor.assetdb._url(p);
-            results.push({ url: url, uuid: Editor.assetdb._path2uuid[p] });
-        }
-        results.sort( function ( a, b ) {
-            return a.url.localeCompare(b.url);
-        });
-        reply(results);
-    },
+  'asset-db-debugger:query-info' ( event, reply ) {
+    var results = [];
+    for ( var p in Editor.assetdb._path2uuid ) {
+      var url = Editor.assetdb._url(p);
+      results.push({ url: url, uuid: Editor.assetdb._path2uuid[p] });
+    }
+    results.sort( function ( a, b ) {
+      return a.url.localeCompare(b.url);
+    });
+    reply(results);
+  },
 };
