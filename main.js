8e06e4f1d3cdbde137230b1eac61111dce60d254
Johnny Wu <KarasAya@gmail.com>
2016-02-14 17:54:17 +0800

    update main process registry

--- main.js
+++ main.js
@@ -7,19 +7,21 @@ module.exports = {
   unload () {
   },
 
-  'asset-db-debugger:open' (event, uuid) {
-    Editor.Panel.open('asset-db-debugger.panel');
-  },
+  messages: {
+    open () {
+      Editor.Panel.open('asset-db-debugger.panel');
+    },
 
-  'asset-db-debugger:query-info' ( event, reply ) {
-    var results = [];
-    for ( var p in Editor.assetdb._path2uuid ) {
-      var url = Editor.assetdb._url(p);
-      results.push({ url: url, uuid: Editor.assetdb._path2uuid[p] });
-    }
-    results.sort( function ( a, b ) {
-      return a.url.localeCompare(b.url);
-    });
-    reply(results);
+    'query-info' ( event, reply ) {
+      var results = [];
+      for ( var p in Editor.assetdb._path2uuid ) {
+        var url = Editor.assetdb._url(p);
+        results.push({ url: url, uuid: Editor.assetdb._path2uuid[p] });
+      }
+      results.sort( function ( a, b ) {
+        return a.url.localeCompare(b.url);
+      });
+      reply(results);
+    },
   },
 };
